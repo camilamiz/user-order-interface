62b8b624bc55590b7e860511ad814cecdc8e597f
camila mizumoto <camila.yuri.m@gmail.com>
2018-12-07 20:52:41 -0200

    Create orders controller method and edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
   get 'orders/new'
   get 'orders/create'
   root to: 'pages#home'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [ :index, :show, :create ]
+      resources :users, only: [ :index, :show, :new, :create ] do
+        resources :orders, only: [ :new, :create ]
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
