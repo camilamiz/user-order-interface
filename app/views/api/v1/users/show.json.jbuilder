bba4076ffb13e98e673cbc7d665017e0866df294
camila mizumoto <camila.yuri.m@gmail.com>
2018-12-10 09:43:43 -0200

    Add parsing logic to userx and orderx

--- app/views/api/v1/users/show.json.jbuilder
+++ app/views/api/v1/users/show.json.jbuilder
@@ -1,4 +1,4 @@
 json.extract! @user, :id, :name, :cpf, :email
 json.comments @user.orders do |order|
-  json.extract! order, :id, :model, :imei, :annual_price, :number_of_installments
+  json.extract! order, :id, :model, :imei, :annual_price, :number_of_installments, :user_id
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,39 @@
 require 'json'
 require 'open-uri'
 
-url = 'http://localhost:3000/api/v1/users'
-user_serialized = open(url).read
-user = JSON.parse(user_serialized)
 
-puts "#{user[0]['name']}"
+puts "Parse users..."
+
+url = 'http://localhost:3000/api/v1/users'
+users_serialized = open(url).read
+users = JSON.parse(users_serialized)
+
+users.each do |user|
+  Userx.create!(name: user['name'], cpf: user['cpf'], email: user['email'])
+end
+
+puts "Finished parsing users!"
+
+
+puts "Parse orders..."
+
+user_id = Userx.pluck(:id)
+
+user_id.each do |id|
+  url = "http://localhost:3000/api/v1/users/#{id}"
+  orders_serialized = open(url).read
+  orders = JSON.parse(orders_serialized)
+  if orders['comments'][0].nil?
+  else
+    orders['comments'].each_with_index do |order, index|
+      Orderx.create!(
+        model: orders['comments'][index]['model'],
+        imei: orders['comments'][index]['imei'],
+        annual_price: orders['comments'][index]['annual_price'],
+        number_of_installments: orders['comments'][index]['number_of_installments'],
+        user_id: orders['comments'][index]['user_id'])
+    end
+  end
+end
+
+puts "Finished parsing users!"
